57ee098a4737dbcb72550d899bb8a12663e014e0
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2026-08-23 16:30:02 +0100

    fix: router hexRegister

--- src/router/index.ts
+++ src/router/index.ts
@@ -136,6 +136,7 @@ async function hexReset (_to: RouteLocationNormalized, _from: RouteLocationNorma
 async function hexRegister (to: RouteLocationNormalized, _from: RouteLocationNormalized): Promise<string> {
 	if (to.params.id?.length !== 128) {
 		snackError({ message: 'Invalid verification data' })
+		return FrontEndRoutes.BASE
 	}
 	const success = await fetch_incognito.verify_get(String(to.params.id))
 	if (success) {
