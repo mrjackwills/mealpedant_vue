099949a70ace66d903ad4093952d6d070e3244fd
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2026-08-25 09:49:10 +0100

    fix: ResetView validations

--- src/views/HexAuthenticated/ResetView.vue
+++ src/views/HexAuthenticated/ResetView.vue
@@ -27,7 +27,6 @@
 							/>
 
 							<v-expand-transition>
-								<PasswordContainsEmail v-if='errors.new_password && !passwordCompromised' />
 								<HibpMessage v-if='passwordCompromised' />
 							</v-expand-transition>
 
@@ -191,10 +190,10 @@ async function reset (): Promise<void> {
 watch(watcher_password, () => {
 	passwordCompromised.value = false
 	errors.value.new_password = false
-	if (!v$.value.user?.new_password?.$invalid && !passwordCompromised.value) errorMessages.value.new_password = ''
-	else if (!v$.value.user?.new_password?.$dirty) return
-	else if (!v$.value.user?.new_password?.required) errorMessages.value.new_password = 'password required'
-	else if (!v$.value.user?.new_password?.minLength) errorMessages.value.new_password = '12 characters minimum'
+	if (!v$.value.new_password.$invalid && !passwordCompromised.value) errorMessages.value.new_password = ''
+	else if (!v$.value.new_password.$dirty) return
+	else if (v$.value.new_password.required.$invalid) errorMessages.value.new_password = 'password required'
+	else if (v$.value.new_password.minLen.$invalid) errorMessages.value.new_password = '12 characters minimum'
 })
 
 const rules = {
